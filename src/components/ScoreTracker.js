7cad58670627af97f5a760c9f0e5822ac766123f
Tyson Dravey <github@12dok.com>
2024-10-22 16:04:17 -0400

    Fixed iOS layout issues and safe areas

--- src/components/ScoreTracker.js
+++ src/components/ScoreTracker.js
@@ -133,7 +133,7 @@ function ScoreTracker() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-4 space-y-4">
+    <div className="max-w-2xl mx-auto p-4 space-y-4" style={{ paddingTop: 'calc(env(safe-area-inset-top) + 1rem)' }}>
       {/* Home View */}
       {view === 'home' && (
         <>
