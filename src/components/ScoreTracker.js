ffd79778485200a9c96a6241d38c24e8a5949a2d
Tyson Dravey <github@12dok.com>
2024-10-22 16:07:13 -0400

    Increased top padding for better iOS layout

--- src/components/ScoreTracker.js
+++ src/components/ScoreTracker.js
@@ -133,7 +133,12 @@ function ScoreTracker() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-4 space-y-4" style={{ paddingTop: 'calc(env(safe-area-inset-top) + 1rem)' }}>
+    <div className="max-w-2xl mx-auto p-4 space-y-4" style={{ 
+        paddingTop: 'calc(env(safe-area-inset-top) + 4rem)',  // Increased from 1rem to 4rem
+        paddingBottom: 'env(safe-area-inset-bottom)',
+        paddingLeft: 'env(safe-area-inset-left)',
+        paddingRight: 'env(safe-area-inset-right)'
+      }}>
       {/* Home View */}
       {view === 'home' && (
         <>
