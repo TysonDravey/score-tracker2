4fdc7eba03eda87980c2c62f3f326967c43831e6
Tyson Dravey <github@12dok.com>
2024-10-23 16:30:21 -0400

    added proper return

--- src/components/ScoreTracker.js
+++ src/components/ScoreTracker.js
@@ -958,4 +958,21 @@ function ScoreTracker() {
       </button>
     </div>
   );
+
+  // Main return statement
+  return (
+    <div
+      className="max-w-2xl mx-auto p-4 space-y-4"
+      style={{ paddingTop: "5rem" }}
+    >
+      {view === "home" && renderHome()}
+      {view === "players" && renderPlayers()}
+      {view === "newGame" && renderNewGame()}
+      {view === "game" && renderGame()}
+      {view === "settings" && renderSettings()}
+    </div>
+  );
 }
+
+// Add the export statement
+export default ScoreTracker;
