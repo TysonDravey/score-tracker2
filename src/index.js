3a1477db012c68c343c31728481f69a6ef306bfd
Tyson Dravey <github@12dok.com>
2024-10-23 09:17:01 -0400

    Simplified splash screen implementation with debug logging

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ScoreTracker from './components/ScoreTracker';
+
+console.log('React app starting...');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ScoreTracker />
   </React.StrictMode>
 );
+
+console.log('React render called');
